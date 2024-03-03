03452fe30865a5685b9fda0c1870235d030c3435
ndl0901@gmail.com <ndl0901@gmail.com>
2024-03-03 20:19:52 +0700

    Test route all

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -11,30 +11,31 @@ class RouteController extends Controller
 
 		// $subdomain = $data['subdomain'];
 		$ada = 0;
-		$user = User::where('subdomain', $request->subdomain)->get();
-		foreach($user as $val){
-			$main = 'false';
-			if($val->main == 1){
-				$main = 'true';
-			}
-			$ada = 1;
-			$response = response()->json([
-				'id' => $val->id,
-				'tenant_id' => $val->subdomain,
-				'main' => $main,
-				'email' => $val->email,
-				'name' => $val->name,
-				'logoURL' => $val->logoUrl,
-			]);
-			return $response;
-		}
-		if($ada == 0){
-			return $response = response()->json([
-				'Message'=> 'Not Found',
-				'subdomain' => $request->subdomain,
-				'request' => $request,
-			]);
-		}
+		$user = User::all();
+        return response()->json($user);
+		// foreach($user as $val){
+		// 	$main = 'false';
+		// 	if($val->main == 1){
+		// 		$main = 'true';
+		// 	}
+		// 	$ada = 1;
+		// 	$response = response()->json([
+		// 		'id' => $val->id,
+		// 		'tenant_id' => $val->subdomain,
+		// 		'main' => $main,
+		// 		'email' => $val->email,
+		// 		'name' => $val->name,
+		// 		'logoURL' => $val->logoUrl,
+		// 	]);
+		// 	return $response;
+		// }
+		// if($ada == 0){
+		// 	return $response = response()->json([
+		// 		'Message'=> 'Not Found',
+		// 		'subdomain' => $request->subdomain,
+		// 		'request' => $request,
+		// 	]);
+		// }
 	}
 }
 ?>
